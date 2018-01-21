76e316030178086819c487face318b22486a361b
Shepo6 <philshepo@gmail.com>
2018-01-21 13:33:16 +0000

    update1

--- addons_xml_generator.py
+++ addons_xml_generator.py
@@ -1,8 +1,30 @@
+# *
+# *  Copyright (C) 2012-2013 Garrett Brown
+# *  Copyright (C) 2010      j48antialias
+# *
+# *  This Program is free software; you can redistribute it and/or modify
+# *  it under the terms of the GNU General Public License as published by
+# *  the Free Software Foundation; either version 2, or (at your option)
+# *  any later version.
+# *
+# *  This Program is distributed in the hope that it will be useful,
+# *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+# *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# *  GNU General Public License for more details.
+# *
+# *  You should have received a copy of the GNU General Public License
+# *  along with XBMC; see the file COPYING.  If not, write to
+# *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+# *  http://www.gnu.org/copyleft/gpl.html
+# *
+# *  Based on code by j48antialias:
+# *  https://anarchintosh-projects.googlecode.com/files/addons_xml_generator.py
+
 """ addons.xml generator """
- 
+
 import os
 import sys
- 
+
 # Compatibility with 3.0, 3.1 and 3.2 not supporting u"" literals
 if sys.version < '3':
     import codecs
@@ -11,7 +33,7 @@ if sys.version < '3':
 else:
     def u(x):
         return x
- 
+
 class Generator:
     """
         Generates a new addons.xml file from each addons addon.xml file
@@ -24,7 +46,7 @@ class Generator:
         self._generate_md5_file()
         # notify user
         print("Finished updating addons xml and md5 files")
- 
+    
     def _generate_addons_file( self ):
         # addon list
         addons = os.listdir( "." )
@@ -38,7 +60,7 @@ class Generator:
                 # create path
                 _path = os.path.join( addon, "addon.xml" )
                 # split lines for stripping
-                xml_lines = open( _path, "r" , encoding="UTF-8").read().splitlines()
+                xml_lines = open( _path, "r" ).read().splitlines()
                 # new addon
                 addon_xml = ""
                 # loop thru cleaning each line
@@ -59,7 +81,7 @@ class Generator:
         addons_xml = addons_xml.strip() + u("\n</addons>\n")
         # save file
         self._save_file( addons_xml.encode( "UTF-8" ), file="addons.xml" )
- 
+    
     def _generate_md5_file( self ):
         # create a new md5 hash
         try:
@@ -68,14 +90,14 @@ class Generator:
         except ImportError:
             import hashlib
             m = hashlib.md5( open( "addons.xml", "r", encoding="UTF-8" ).read().encode( "UTF-8" ) ).hexdigest()
- 
+        
         # save file
         try:
             self._save_file( m.encode( "UTF-8" ), file="addons.xml.md5" )
         except Exception as e:
             # oops
             print("An error occurred creating addons.xml.md5 file!\n%s" % e)
- 
+    
     def _save_file( self, data, file ):
         try:
             # write data to the file (use b for Python 3)
@@ -83,8 +105,8 @@ class Generator:
         except Exception as e:
             # oops
             print("An error occurred saving %s file!\n%s" % ( file, e ))
- 
- 
+
+
 if ( __name__ == "__main__" ):
     # start
     Generator()
